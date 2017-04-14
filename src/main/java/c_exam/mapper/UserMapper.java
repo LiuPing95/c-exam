72c45ad593d53918243a7225b6c0c266d4b4b4ef
LiuPing <465638135@qq.com>
2017-04-14 22:28:23 +0800

    userMapper增加接口

--- src/main/java/c_exam/mapper/UserMapper.java
+++ src/main/java/c_exam/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package c_exam.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import c_exam.pojo.dao.UserInfo;
@@ -11,4 +13,20 @@ import c_exam.pojo.dao.UserInfo;
 public interface UserMapper {
 
 	void addUser(@Param("obj") UserInfo obj);
+
+	/**
+	 * 分页查询数据，TODO：后面还要支持多条件分页查询
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	List<UserInfo> getUsers(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize);
+	
+	/**
+	 * 根据userId精确查找用户
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	UserInfo getUserById(@Param("userId") Integer userId);
 }
